6b43f21bb919e39c0f358e7a6a2ecd96e63db98b
Jeric Montaña <mntnjrc@gmail.com>
2024-10-28 22:21:08 +0800

    update webpack config to handle not existing JS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
 const path = require('path');
+const fs = require('fs');
 const TerserPlugin = require('terser-webpack-plugin');
+
+// Function to check if a file exists
+const fileExists = (filePath) => fs.existsSync(path.resolve(__dirname, filePath));
+
+// Define entry points, including checks to ensure the files exist
+const entryPoints = {
+  'kikstart-custom': './src/js/kikstart-custom.js',
+  'kik-collect-lazy-assets.min': './src/js/kik-collect-lazy-assets.js',
+  'kik-load-lazy-assets.min': './src/js/kik-load-lazy-assets.js'
+  // Add other entry points if needed
+};
+
+// Filter out non-existing entry points
+const validEntries = Object.fromEntries(
+  Object.entries(entryPoints).filter(([key, filePath]) => fileExists(filePath))
+);
+
 module.exports = {
-  entry: {
-    'kikstart-custom': './src/js/kikstart-custom.js',
-    'kik-collect-lazy-assets.min': './src/js/kik-collect-lazy-assets.js',
-    'kik-load-lazy-assets.min': './src/js/kik-load-lazy-assets.js'
-    // Add other entry points if needed
-  },
+  entry: validEntries,
   output: {
     path: path.resolve(__dirname, './assets'),
     filename: '[name].js',
-    // filename: 'kikstart-custom.js',
   },
   mode: 'production',
   devtool: false,
